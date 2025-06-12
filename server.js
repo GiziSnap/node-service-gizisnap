f522b39b2c45ed138dd587f1d3ae1447f3d90248
han <icyfarhan@gmail.com>
2025-06-12 21:39:31 +0700

    Newest update to the server.js

--- server.js
+++ server.js
@@ -293,8 +293,8 @@ const init = async () => {
   ]);
 
   const server = Hapi.server({
-    port: 3000,
-    host: "0.0.0.0",
+    port: 3001,
+    host: "localhost",
     routes: {
       cors: { origin: ["*"], credentials: true },
       payload: {
